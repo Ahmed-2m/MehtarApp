898e7f7cf443280915c83e8ae6118af88cf6cf28
Ahmed-2m <777366634xxahmedxx@gmail.com>
2026-08-08 23:07:03 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MehtarApp());
+}
+
+class MehtarApp extends StatelessWidget {
+  const MehtarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'محتار',
+      debugShowCheckedModeBanner: false, // لإخفاء شريط التجربة الإعلاني
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        scaffoldBackgroundColor: const Color(
+          0xFFF8F9FA,
+        ), // لون خلفية مريح وأنيق
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'تطبيق محتار 🍕',
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
